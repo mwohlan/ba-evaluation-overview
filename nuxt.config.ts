58ce67b7c10e860f1fd94667c663d52d548de5b6
Markus Wohlan <markuswohlan@gmail.com>
2022-04-06 23:05:07 +0200

    remove preset lambda

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -58,7 +58,5 @@ export default defineNuxtConfig({
       ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
     ],
   },
-  nitro: {
-    preset: 'lambda',
-  },
+
 })
